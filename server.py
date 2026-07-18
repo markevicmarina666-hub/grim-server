d8c7492ddc953aa7b73f3e6082200a90595f9e6d
markevicmarina666-hub <markevicmarina666@gmail.com>
2026-07-18 15:48:05 +0300

    Update server.py

--- server.py
+++ server.py
@@ -84,7 +84,8 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.send_header('Access-Control-Allow-Origin', '*')
         super().end_headers()
 
-def run_http():
+def run_http():print("FILES:", os.listdir("."))
+print("ROOT:", os.listdir("/opt/render/project/src"))
     os.chdir("/opt/render/project/src")
     with socketserver.TCPServer(("0.0.0.0", HTTP_PORT), Handler) as httpd:
         print(f"HTTP on {HTTP_PORT}")
